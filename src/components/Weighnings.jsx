24190de6d35c544a6bae15c77785db473579f430
kylusek <kylukylusek5@gmail.com>
2023-07-06 15:51:18 +0200

    Button hover added, array items order still not fixed

--- src/components/Weighnings.jsx
+++ src/components/Weighnings.jsx
@@ -11,6 +11,7 @@ export default function Weighnings(props) {
 			.then(res => res.json())
 			.then((data) => {
 				setPosts(data);
+				console.log(data.Scales[0].Weighnings[0].weight, data.Scales[0].Weighnings[1].weight);
 			})
 			.catch((err) => {
 				console.log(err.message);
