3f7b2152bda735a96b30b407dd9d50504c2a988e
kylusek <kylukylusek5@gmail.com>
2023-07-07 11:29:47 +0200

    Unused variables remove

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,13 @@
-import { useState, useEffect } from "react";
 import Header from "./components/Header"
 import Weighnings from "./components/Weighnings";
 import './styles/main.scss'
 import './styles/responsive.scss'
 
 export default function App() {
-	const [columnCount, setColumnCount] = useState(6);
-
 	return (
 		<>
 			<Header />
-			<Weighnings columnCount={columnCount}/>
+			<Weighnings/>
 		</>
 	)
 }
